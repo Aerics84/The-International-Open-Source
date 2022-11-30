31e5ce5c11592c269bad576a8168e0c2b71a3207
Aerics <sascha.licht@realityworld.de>
2022-11-30 07:34:31 +0100

    small fixes

--- src/room/commune/haulerNeedManager.ts
+++ src/room/commune/haulerNeedManager.ts
@@ -13,8 +13,7 @@ export class HaulerNeedManager {
         for (let index in room.sources) {
             if (room.sourceLinks[index]) continue
 
-            room.haulerNeed +=
-                findCarryPartsRequired(room.sourcePaths[index].length, room.estimatedSourceIncome[index]) * 1.4
+            room.haulerNeed += findCarryPartsRequired(room.sourcePaths[index].length, room.estimatedSourceIncome[index])
         }
 
         if (!room.controllerLink)
