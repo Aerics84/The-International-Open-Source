5aa9870a3026bb1f046dd6defb0479b550dcb27c
Aerics <sascha.licht@realityworld.de>
2023-01-22 13:44:55 +0100

    more haulerNeed

--- src/room/commune/haulerNeed.ts
+++ src/room/commune/haulerNeed.ts
@@ -30,7 +30,7 @@ export class HaulerNeedManager {
 
         room.haulerNeed += room.structures.lab.length / 1.5
 
-        /* room.haulerNeed += room.structures.extension.length / 10 */
+        room.haulerNeed += room.structures.extension.length / 10
 
         if ((room.controller.level >= 4 && room.storage) || (room.terminal && room.controller.level >= 6))
             room.haulerNeed += Memory.stats.rooms[room.name].eosp / 50
